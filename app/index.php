a61c500831a239f3fc37e4a05ad3d73986fe32d6
Jack Vial <vialjack@mail.com>
2014-10-22 06:50:18 -0400

    Auto complete and ajax working

--- app/index.php
+++ app/index.php
@@ -1,7 +1,23 @@
 <?php
 
 
-	 $items = ["framework" => "Agavi","AIDA Web","Akelos","Apache Click","Apache Cocoon","Apache Struts","Apache Wicket","AppFuse","Aranea","ASP.NET MVC","Axiom Stack","BFC","CakePHP","DooPHP","Camping","Catalyst","CherryPy","CodeIgniter","ColdSpring","Csla","CppCMS","CubicWeb","Django","Drupal","DotNetNuke","eZ Components","Flex","FUSE","Fusebox","Google Web Toolkit","Grok","Grails","Hamlets","Helix","Horde","Interchange","ItsNat","IT Mill Toolkit","JavaServer Faces","JBoss Seam","Kepler","Kohana","KumbiaPHP","Lift","LISA","ManyDesigns Portofino","Mason","Maypole","Mach-II","Merb","Midgard","Model-Glue","MonoRail","Morfik","Nitro","onTap","OpenACS","OpenLaszlo","OpenXava","Orbit","Orinoco","PEAR","PHP For Applications","PHP Work","Pyjamas","Pylons","Qcodo","RIFE","Ruby on Rails","Samstyle","Seaside","Shale","SilverStripe (Sapphire)","Simplicity","Sinatra","SmartClient","Sofia","SPIP","Spring","Stripes","Symfony","Tapestry","ThinWire","Tigermouse","Vaadin","TurboGears","Wavemaker","web2py","WebObjects","WebWork","Wigbi","Yii","Zend","ZK","Zoop","Zope 2","Zope 3","ztemplates"]; 
+	 $items = [["framework" => "Agavi"],
+	 		["framework" => "AIDA Web"],
+	 		["framework" => "Akelos"],
+	 		["framework" => "Apache Wicket"],
+	 		["framework" => "AppFuse"],
+	 		["framework" => "Aranea"],
+	 		["framework" => "ASP.NET MVC"],
+	 		["framework" => "Axiom Stack"],
+	 		["framework" => "BFC"],
+	 		["framework" => "CakePHP"],
+	 		["framework" => "DooPHP"],
+	 		["framework" =>"Camping"],
+	 		["framework" => "Catalyst"],
+	 		["framework" =>"CherryPy"],
+	 		["framework" =>"CodeIgniter"]];
+
+	 //"Apache Wicket","AppFuse","Aranea","ASP.NET MVC","Axiom Stack","BFC","CakePHP","DooPHP","Camping","Catalyst","CherryPy","CodeIgniter","ColdSpring","Csla","CppCMS","CubicWeb","Django","Drupal","DotNetNuke","eZ Components","Flex","FUSE","Fusebox","Google Web Toolkit","Grok","Grails","Hamlets","Helix","Horde","Interchange","ItsNat","IT Mill Toolkit","JavaServer Faces","JBoss Seam","Kepler","Kohana","KumbiaPHP","Lift","LISA","ManyDesigns Portofino","Mason","Maypole","Mach-II","Merb","Midgard","Model-Glue","MonoRail","Morfik","Nitro","onTap","OpenACS","OpenLaszlo","OpenXava","Orbit","Orinoco","PEAR","PHP For Applications","PHP Work","Pyjamas","Pylons","Qcodo","RIFE","Ruby on Rails","Samstyle","Seaside","Shale","SilverStripe (Sapphire)","Simplicity","Sinatra","SmartClient","Sofia","SPIP","Spring","Stripes","Symfony","Tapestry","ThinWire","Tigermouse","Vaadin","TurboGears","Wavemaker","web2py","WebObjects","WebWork","Wigbi","Yii","Zend","ZK","Zoop","Zope 2","Zope 3","ztemplates"]; 
 
 	 echo json_encode($items);
 
